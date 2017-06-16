f915784b707e127519009233952100f13fbb5d13
Sebastian Andil <sebastian.andil@gmail.com>
2017-06-16 17:22:54 +0200

    add normalize.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Swatch from './components/Swatch';
 
+import 'normalize.css';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <Swatch />
-    );
-  }
+	render() {
+		return (
+			<Swatch />
+		);
+	}
 }
 
 export default App;
