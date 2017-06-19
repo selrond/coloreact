ef68a028c3e31bb1a2fccaa8c394971684f9dedd
Pavol Madár <p.madar@adsulting.com>
2017-06-19 22:17:50 +0200

    onClick handler added

--- src/components/Generate.js
+++ src/components/Generate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import styled from 'styled-components';
 
 const GenerateIconWrapper = styled.div`
@@ -11,14 +12,18 @@ const GenerateIcon = styled.svg`
 	height: auto;
 `;
 
-const Generate = () => {
+const Generate = ({ onClick }) => {
 	return (
-		<GenerateIconWrapper>
+		<GenerateIconWrapper onClick={onClick}>
 			<GenerateIcon xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 				<path fill="none" stroke="#FFF" strokeWidth="2" d="M16 3h5v5M4 20L21 3m0 13v5h-5m-1-6l6 6M4 4l5 5" strokeLinecap="round" strokeLinejoin="round"/>
 			</GenerateIcon>
 		</GenerateIconWrapper>
 	)
+}
+
+Generate.propTypes = {
+	onClick: PropTypes.func.isRequired
 }
 
 export default Generate;
