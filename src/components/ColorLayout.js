4f1bb8753ee1b492e8fd14b04de5226a7048f684
Sebastian Andil <sebastian.andil@gmail.com>
2017-06-19 15:14:50 +0200

    include generate svg icon inline, correct styles

--- src/components/ColorLayout.js
+++ src/components/ColorLayout.js
@@ -3,9 +3,12 @@ import Generate from './Generate'
 import styled from 'styled-components';
 
 const ColorBox = styled.div`
+	background-color: palevioletred;
 	width: 400px;
 	height: 400px;
-	background-color: palevioletred;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
 
 class ColorLayout extends Component {
--- src/components/Generate.js
+++ src/components/Generate.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import shuffle from '../img/shuffle.svg';
 
-const GenerateIcon = styled.div`
-	width: 20px;
-	height: 20px;
+const GenerateIconWrapper = styled.div`
+	width: 9%;
+	height: 9%;
+	cursor: pointer;
 `
+const GenerateIcon = styled.svg`
+	width: 100%;
+	height: auto;
+`;
 
 const Generate = () => {
 	return (
-		<GenerateIcon>
-			<img src={shuffle} alt="shuffle" />
-		</GenerateIcon>
+		<GenerateIconWrapper>
+			<GenerateIcon xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+				<path fill="none" stroke="#FFF" strokeWidth="2" d="M16 3h5v5M4 20L21 3m0 13v5h-5m-1-6l6 6M4 4l5 5" strokeLinecap="round" strokeLinejoin="round"/>
+			</GenerateIcon>
+		</GenerateIconWrapper>
 	)
 }
 
