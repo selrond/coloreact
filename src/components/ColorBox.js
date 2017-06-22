78882f8f524993056c1a3845d39e95fc2b343449
Sebastian Andil <sebastian.andil@gmail.com>
2017-06-22 23:25:55 +0200

    add subtle transition of background-color

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 const ColorLayout = styled.div`
 	background-color: ${props => props.exactColor};
+	transition: background-color .3s ease-in-out;
 	width: 400px;
 	height: 400px;
 	display: flex;
